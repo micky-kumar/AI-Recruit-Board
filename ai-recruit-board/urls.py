85efb000673e31add28a699fba87bd60213706b3
Micky <mickykumar510@gmail.com>
2021-10-17 16:50:00 -0400

    Add jobs in the dashboard

--- ai-recruit-board/urls.py
+++ ai-recruit-board/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from django.contrib import admin
 from apps.core.views import frontpage, signup
 from django.contrib.auth import views
-from apps.job.views import job_detail
+from apps.job.views import job_detail, add_job
 from apps.userprofile.views import dashboard
 
 # To add a new path, first import the app:
@@ -20,6 +20,7 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path("admin/", admin.site.urls),
-    path("jobs/<int:job_id>/", job_detail, name='job_detail'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('jobs/', include('apps.job.urls')),
+    path('dashboard/', include('apps.userprofile.urls')),
+
 ]
--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -6,6 +6,9 @@ class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     is_employer = models.BooleanField(default=False)
 
+    def __str__(self):
+        return str(self.user)
+
 
 
 User.userprofile = property(lambda u:Userprofile.objects.get_or_create(user=u)[0])
