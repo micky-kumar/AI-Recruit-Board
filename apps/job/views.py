7185c5d1b6e0723c32b2c10122f2103e49f4af9b
Micky <mickykumar510@gmail.com>
2021-10-23 19:37:28 -0400

    Applying for jobs

--- apps/job/views.py
+++ apps/job/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import Job
-from .forms import AddJobForm
+from .forms import AddJobForm, ApplicationForm
 
 
 def job_detail(request, job_id):
     job =Job.objects.get(pk=job_id)
 
     return render(request, 'job/job_detail.html', {'job': job})
+
+@login_required
+def apply_for_job(request, job_id):
+    job = Job.objects.get(pk=job_id)
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+
+        if form.is_valid():
+            application = form.save(commit=False)
+            application.created_by = request.user
+            application.save()
+
+            return redirect('dashboard')
+
+    else:
+        form = ApplicationForm()
+
+    return  render(request, 'job/apply_for_job.html',{'form': form, 'job':job})
 
 @login_required
 def add_job(request):
